eb61b100b28145cef02140e36b0d676de352832a
erichiii <angel.letada1205@gmail.com>
2025-10-06 21:53:57 +0800

    created 3x4 tiles placeholder

---
+++ app/src/main/java/com/example/paw_pair_memory_game/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.paw_pair_memory_game
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
